ac301152f6eb3272f580631b14aaa8bd3b6b8c49
Joana Cruz <joana_c_ca@hotmail.com>
2021-12-10 19:46:12 +0000

    added a new route to do the search in the site

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -8,6 +8,8 @@ const { isAuthenticated } = require("./../middleware/jwt.middleware");
 const apiKey = '?api_key=d131d9c2e1e71a17ab471e5e07730c7c'
 const apiURL = 'https://api.themoviedb.org/3/movie';
 const getPopularURL = `${apiURL}/popular${apiKey}`
+const searchURL = `https://api.themoviedb.org/3/search/movie${apiKey}&query=`
+
 
 //GET /popularMovies
 router.get('/api/popularMovies', isAuthenticated, async (req, res, next) => {
@@ -19,7 +21,9 @@ router.get('/api/popularMovies', isAuthenticated, async (req, res, next) => {
             title: movie.title,
             voteAverage: movie.vote_average,
             id: movie.id,
-            posterPath: movie.poster_path
+            posterPath: movie.poster_path,
+            releaseDate: movie.release_date,
+            popularity: movie.popularity
         }))
 
         res.status(200).json(movies);
@@ -29,7 +33,6 @@ router.get('/api/popularMovies', isAuthenticated, async (req, res, next) => {
 })
 
 //GET /myMoviesList/:moviesId
-
 router.get('/api/movie/:movieId', isAuthenticated, async (req, res, next) => {
     try {
         const { movieId } = req.params;
@@ -42,7 +45,9 @@ router.get('/api/movie/:movieId', isAuthenticated, async (req, res, next) => {
             voteAverage: details.vote_average,
             id: details.id,
             posterPath: details.poster_path,
-            overview: details.overview
+            overview: details.overview,
+            releaseDate: details.release_date,
+            popularity: details.popularity
         }
 
         res.status(200).json(movieDetails);
@@ -51,6 +56,26 @@ router.get('/api/movie/:movieId', isAuthenticated, async (req, res, next) => {
     }
 })
 
+//GET /api/movies/search/:query
+router.get('/api/movies/search/:query', isAuthenticated, async (req, res, next) => {
+    try {
+        const { query } = req.params;
+        const response = await axios.get(`${searchURL}${query}`);
+        const searchedMovieList = response.data;
+        console.log(searchedMovieList);
+        const movies = searchedMovieList.results.map(movie => ({
+            title: movie.title,
+            voteAverage: movie.vote_average,
+            id: movie.id,
+            posterPath: movie.poster_path,
+            releaseDate: movie.release_date,
+            popularity: movie.popularity
+        }))
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 
 module.exports = router;
