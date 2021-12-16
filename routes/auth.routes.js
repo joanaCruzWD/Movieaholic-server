c53aecfd5e50d19adacd938c00dfd336bdb8266e
Joana Cruz <joana_c_ca@hotmail.com>
2021-12-16 23:47:55 +0000

    fix typo

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,7 +10,6 @@ const saltRounds = 10;
 
 // POST /auth/signup
 router.post("/auth/signup", async (req, res, next) => {
-  b
   try {
     // Get the data from req.body
     const { email, password, name, image } = req.body;
